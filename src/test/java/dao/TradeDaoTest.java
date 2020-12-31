6e6b54b58b0bb20fc00ad2ae5c73b68caa6104c6
Finger <Finger@spianmo.com>
2020-12-31 13:46:53 +0800

    Trade订单的Dao层及其Impl实现类完成，Trade的Service层Class建立，Dao工厂类和Service工厂类完成

---
+++ src/test/java/dao/TradeDaoTest.java
@@ -0,0 +1,32 @@
+package dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TradeDaoTest {
+
+    @Test
+    void selectAllTrade() {
+    }
+
+    @Test
+    void selectTradeByCustomerId() {
+    }
+
+    @Test
+    void selectTradeByProductId() {
+    }
+
+    @Test
+    void selectTradeById() {
+    }
+
+    @Test
+    void insertTrade() {
+    }
+
+    @Test
+    void deleteTrade() {
+    }
+}
