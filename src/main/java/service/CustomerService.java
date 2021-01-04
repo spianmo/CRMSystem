30d430ae9824dfa426ec94299e4ad86ff97aaa6b
Finger <Finger@spianmo.com>
2021-01-04 09:30:15 +0800

    完成Employee员工面板下客户信息功能

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,4 +1,23 @@
 package service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import entity.Customer;
+
 public interface CustomerService {
+    /**
+     * 查询所有的客户
+     * @return 所有的客户实体集
+     * @throws SQLException SQL查询异常
+     */
+    List<Customer> selectAll();
+
+    /**
+     * 根据员工ID查询其所属的所有客户
+     * @param employeeId 员工ID
+     * @return 某员工所有客户的实体集
+     * @throws SQLException SQL查询异常
+     */
+    List<Customer> selectCustomerByEmployeeId(int employeeId);
 }
--- src/test/java/dao/TradeDaoTest.java
+++ src/test/java/dao/TradeDaoTest.java
@@ -2,12 +2,17 @@ package dao;
 
 import org.junit.jupiter.api.Test;
 
+import factory.DaoFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradeDaoTest {
 
+    TradeDao tradeDao = DaoFactory.getTradeDaoInstance();
+
     @Test
     void selectAllTrade() {
+        System.out.println(tradeDao.selectAllTrade());
     }
 
     @Test
